6ac65b26a9ed48fe7adac545f7d713a50e364b4d
yunuki <woogie_ing@naver.com>
2021-10-08 15:13:02 +0900

    update

--- Source/General/SegementSlideViewController+scroll.swift
+++ Source/General/SegementSlideViewController+scroll.swift
@@ -78,7 +78,7 @@ extension SegementSlideViewController {
                 canParentViewScroll = true
             }
         case .child:
-            if !canChildViewScroll, childScrollView.contentOffset.y != childContentOffsetY {
+            if !canChildViewScroll {
                 childScrollView.contentOffset.y = 0
             } else if childContentOffsetY <= 0 {
                 if parentContentOffsetY <= 0 {
